66228540f8dd109b65ddc932f0ca5d954fd478a6
black8123 <black8123.thanx.yall@gmail.com>
2026-06-11 23:14:53 +0900

    style: 講評パネルを半透明に (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lighttable-v7";
+const CACHE = "lighttable-v8";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
