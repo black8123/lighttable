4ff3ac0f56909d2ab4a77c7079388bb000dc66ba
black8123 <black8123.thanx.yall@gmail.com>
2026-06-12 15:12:02 +0900

    ux: 画面タップで講評パネルを閉じる (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lighttable-v9";
+const CACHE = "lighttable-v10";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
