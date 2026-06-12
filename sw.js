3eb74a39bbebc8783f32b110a6bec236e7356da1
black8123 <black8123.thanx.yall@gmail.com>
2026-06-12 15:38:34 +0900

    feat: 講評依頼プロンプトを強化 (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lighttable-v10";
+const CACHE = "lighttable-v11";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
